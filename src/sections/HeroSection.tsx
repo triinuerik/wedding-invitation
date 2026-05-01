ee659ddbaf1dd624c6adf15c3ef59a4bda86f9af
Triinu Erik <triinue96@gmail.com>
2026-05-01 20:26:11 +0200

    Change to new design WIP

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,5 +1,5 @@
 import styles from './HeroSection.module.css'
-import backgroundImage from '../assets/images/background.jpg'
+import backgroundImage from '../assets/images/background.jpeg'
 
 export function HeroSection() {
   return (
@@ -8,9 +8,11 @@ export function HeroSection() {
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className={styles.heroContent}>
-        <h1>We&apos;re Getting Married!</h1>
-        <p className={styles.subtitle}>You&apos;re invited to celebrate with us</p>
+        <h1>Triinu & Konrad</h1>
+        <h2>ARE GETTING MARRIED</h2>
       </div>
+      <p className={styles.date}>24 OCTOBER 2026, 17:00</p>
+      <p className={styles.location}>FOTOGRAFISKA, TALLINN</p>
     </section>
   )
 }
--- src/sections/WelcomeSection.tsx
+++ src/sections/WelcomeSection.tsx
@@ -1,19 +1,46 @@
 import styles from './WelcomeSection.module.css'
+import kissImage from '../assets/images/kiss.jpeg'
 
 export function WelcomeSection() {
   return (
     <section className={styles.welcome}>
-      <p>Dear friends and family,</p>
-      <p>
-        We are delighted to invite you to share in the joy of our wedding as we
-        begin our journey together as husband and wife.
-      </p>
-      <p className={styles.names}>Triinu and Konrad</p>
-      <div className={styles.divider}></div>
-      <p>
-        We would be honoured by your presence and look forward to celebrating
-        this special day with you.
-      </p>
+      <div className={styles.panel}>
+        <div className={styles.imagePane}>
+          <img className={styles.photo} src={kissImage} alt="Triinu and Konrad kissing in nature" />
+        </div>
+        <div className={styles.content}>
+          <p>Dear family and friends,</p>
+          <p>
+            We are so excited to invite you to our wedding party on{' '}
+            <span className={styles.impact}>24.10.2026</span> taking place in the rooftop restaurant in{' '}
+            <span className={styles.impact}>Fotografiska, Tallinn</span>. The party starts at{' '}
+            <span className={styles.impact}>17:00</span>.
+          </p>
+          <p>Please let us know if you will or won&apos;t be joining us by XX.XX.XXXX using the form below.</p>
+          <p>Can&apos;t wait to celebrate with you,</p>
+          <p className={styles.names}>Triinu &amp; Konrad</p>
+        </div>
+      </div>
+
+      <div className={`${styles.panel} ${styles.infoPanel}`}>
+        <div className={styles.content}>
+          <p className={styles.heading}>Gifts and flowers</p>
+          <p>
+            Your presence is truly the best gift we could ask for, but if you feel called to bring a gift, please fit
+            it inside an envelope. We kindly ask you to not bring flowers.
+          </p>
+          <p className={styles.heading}>Children</p>
+          <p>We kindly ask you to leave your little ones at home in the care of a sitter.</p>
+          <p className={styles.heading}>Anything we didn&apos;t cover?</p>
+          <p>
+            We are happy to answer to any questions you might have, feel free to reach out. On the day of the event or
+            if you wish to plan a surprise, you can reach our wedding planner at +372 5555 5555 or xxx@yyy.com
+          </p>
+        </div>
+        <div className={styles.imagePane}>
+          <img className={styles.photo} src={kissImage} alt="Triinu and Konrad standing together in nature" />
+        </div>
+      </div>
     </section>
   )
 }
